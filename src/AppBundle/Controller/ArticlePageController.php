822965ee8920d198439fb1916a1fc2bbe5e8f587
Vladislav Kuznetsov <Kuznets.vlad200@gmail.com>
2022-09-04 19:11:03 +0300

    use path changes

--- src/AppBundle/Controller/ArticlePageController.php
+++ src/AppBundle/Controller/ArticlePageController.php
@@ -7,7 +7,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Service\ArticlesController;
+use AppBundle\Controller\ArticlesController;
 
 class ArticlePageController extends Controller
 {
@@ -19,7 +19,6 @@ class ArticlePageController extends Controller
     public function indexAction($article_id)
     {
         $id = $article_id;
-
         return $this->render('pages/articlePage.html.twig',
         ['id' => $id]);
     }
